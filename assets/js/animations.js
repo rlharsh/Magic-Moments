b6165ae2a8cbc510bbce10184294249490bdf7b0
Ronald Lee Harsh <ronald.harsh@protonmail.com>
2022-12-30 13:54:12 -0600

    Update

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,31 +1,19 @@
 let t = document.getElementById("navi-toggle");
-let l = document.getElementById("page-wrapper");
-
-let minimized = false;
 
 t.addEventListener('change', () => {
     if (t.checked) {
-        let minimized = true;
         let q = document.getElementById("page-wrapper");
         q.classList.add("hide");
         let r = document.getElementById("social-media-container");
         r.classList.add("show-social-media")
+        let s = document.getElementById("logo-container");
+        s.classList.add("show-logo")
     } else {
-        let minimized = false;
         let q = document.getElementById("page-wrapper");
         q.classList.remove("hide");
         let r = document.getElementById("social-media-container");
         r.classList.remove("show-social-media")
-    }
-})
-
-t.addEventListener('click', () => {
-    console.log('min')
-    if (minimized) {
-        let minimized = false;
-        let q = document.getElementById("page-wrapper");
-        q.classList.remove("hide");
-        let r = document.getElementById("social-media-container");
-        r.classList.remove("show-social-media")
+        let s = document.getElementById("logo-container");
+        s.classList.remove("show-logo")
     }
 })
